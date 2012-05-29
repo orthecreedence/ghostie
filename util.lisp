f3381f72dfbf4914b776e01f370c0cf136ee56e2
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-05-28 17:19:11 -0700

    basically converted the game from 2d ortho to 3d layered. looks great so far. added fog. updated the models. once real lighting/grain/particle effects are in, it will look reaaall nice :). then its onto physics.

--- util.lisp
+++ util.lisp
@@ -21,19 +21,31 @@
            (data (make-string len)))
       (values data (read-sequence data s)))))
 
-(defun load-points-from-ai (filename)
+(defun load-points-from-ai (filename &key precision)
   (let ((points nil)
         (in-point-block nil)
-        (file-data (file-string filename)))
+        (file-data (file-string filename))
+        (mult (if precision (expt 10 precision) 1)))
     (loop for line in (split-sequence:split-sequence #\return file-data) do
           (cond ((equal line "1 XR")
                  (setf in-point-block t))
-                ((and (equal line "n") (not (zerop (length points))))
+                ((and (equal (string-downcase line) "n") (not (zerop (length points))))
                  (return))
                 (in-point-block
-                  (let ((verts (read-from-string (concatenate 'string "(" line ")"))))
+                  (let* ((verts (read-from-string (concatenate 'string "(" line ")")))
+                         (verts (if precision 
+                                    (mapcar (lambda (v) (if (numberp v)
+                                                            (/ (floor (* v mult)) mult)
+                                                            nil)) verts)
+                                    verts)))
                     (push (list (car verts) (cadr verts)) points)))))
-    (reverse points)
-    (push '(0 0) points)))
+    (let* ((last (car points))
+           (points (reverse points)))
+      (if (equal (car points) last)
+          (butlast points)
+          points))))
 
-(load-points-from-ai #P"resources/ground.ai")
+(defun object-leftmost-point (points)
+  (reduce (lambda (a b) (min (if (listp a) (car a) a)
+                             (if (listp b) (car b) b))) points))
+
