8a32d2cf1afa799bdd97aa95ee6541535b979a54
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-06-02 03:14:48 -0700

    removing old shaders

--- shaders/fragment.v1.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-out vec4 outputColor;
-void main()
-{
-	outputColor = vec4(0.1f, 0.1f, 0.1f, 1.0f);
-}
-
--- shaders/vertex.v1.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-
-layout(location = 0) in vec3 position;
-void main()
-{
-	gl_Position.xyz = position;
-}
