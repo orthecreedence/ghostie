014fdd7ccb743bd4dc7d3fab8076c57cbe726597
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-06-11 21:34:22 -0700

    switching mack to 2d world

--- input.lisp
+++ input.lisp
@@ -15,11 +15,6 @@
     (decf (nth 0 *world-position*) .2))
   (when (sdl:key= key :sdl-key-r)
     (setf *world-position* '(-17.19999 -24.00002 -36.000065)))
-  (when (sdl:key= key :sdl-key-m)
-    (bt:make-thread (lambda ()
-                      (loop for d from 0 to 1080 by 0.1 do
-                            (setf (gl-object-rotation (getf *game-data* :spike))
-                                  (list 1 0 0 d)) (sleep .003)))))
   (when (sdl:key= key :sdl-key-t)
     (test-gl-funcs))
   (when (sdl:key= key :sdl-key-l)
