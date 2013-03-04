478acf7a7392e144dd1dcdccfb71a88615531bc4
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-03-03 21:55:54 -0800

    implementing allowing collision callbacks to write data into arbiter (at either begin or pre-solve) as well as to ignore a collection in begin or pre-solve (via cpw:arbiter-ignore-collision accessor)

--- physics.lisp
+++ physics.lisp
@@ -12,7 +12,7 @@
           (body-b (cpw:find-body-from-pointer (cp-a:shape-body (cffi:mem-aref b :pointer)))))
       (values body-a body-b))))
 
-(defmacro define-collision-callback (name (body1-var body2-var) &body body)
+(defmacro define-collision-callback (name (arbiter-var body1-var body2-var &key (sync t)) &body body)
   `(defun ,name (arbiter space data)
      (declare (ignore space data))
      ;; let's be careful here since a condition can derail chipmunk and creash
@@ -20,7 +20,12 @@
      (handler-case
        (multiple-value-bind (,body1-var ,body2-var)
            (get-bodies-from-arbiter arbiter)
-         ,@body)
+         (let ((,arbiter-var (cpw:make-arbiter arbiter)))
+           ,@body
+           (when ,sync (cpw:sync-arbiter-to-c ,arbiter-var))
+           (if (cpw:arbiter-ignore-collision ,arbiter-var)
+               cp:+false+
+               cp:+true+)))
        (ccl::invalid-memory-access (e)
          (dbg :error "(physics) Invalid memory access =[ ~a~%" e))
        (error (e)
@@ -30,29 +35,22 @@
 ;; TODO call make-arbiter only once, and store the result in the arbiter c obj
 ;; itself. note that creating a pointer, setting it into a hash table, and
 ;; dereferencing it to get the arbiter data might be slower than just calling
-;; make-arbiter for each collission call (but need to benchmark before assuming)
-;; TODO sync data in arbiter-data object to arbiter c object so collision
-;; callbacks can modify collision values (friction, elasticity, surface
-;; velocity, ignore this collision)
-(define-collision-callback collision-begin (body1 body2)
-  (let ((arbiter-data (cpw:make-arbiter arbiter)))
-    (trigger :collision-begin body1 body2 arbiter-data)
-    (trigger :collision-begin body2 body1 arbiter-data)))
+;; make-arbiter for each collision call (but need to benchmark before assuming)
+(define-collision-callback collision-begin (arbiter-data body1 body2)
+  (trigger :collision-begin body1 body2 arbiter-data)
+  (trigger :collision-begin body2 body1 arbiter-data))
 
-(define-collision-callback collision-pre-solve (body1 body2)
-  (let ((arbiter-data (cpw:make-arbiter arbiter)))
-    (trigger :collision-pre body1 body2 arbiter-data)
-    (trigger :collision-pre body2 body1 arbiter-data)))
+(define-collision-callback collision-pre-solve (arbiter-data body1 body2)
+  (trigger :collision-pre body1 body2 arbiter-data)
+  (trigger :collision-pre body2 body1 arbiter-data))
 
-(define-collision-callback collision-post-solve (body1 body2)
-  (let ((arbiter-data (cpw:make-arbiter arbiter)))
-    (trigger :collision-post body1 body2 arbiter-data)
-    (trigger :collision-post body2 body1 arbiter-data)))
+(define-collision-callback collision-post-solve (arbiter-data body1 body2)
+  (trigger :collision-post body1 body2 arbiter-data)
+  (trigger :collision-post body2 body1 arbiter-data))
 
-(define-collision-callback collision-separate (body1 body2)
-  (let ((arbiter-data (cpw:make-arbiter arbiter)))
-    (trigger :collision-separate body1 body2 arbiter-data)
-    (trigger :collision-separate body2 body1 arbiter-data)))
+(define-collision-callback collision-separate (arbiter-data body1 body2)
+  (trigger :collision-separate body1 body2 arbiter-data)
+  (trigger :collision-separate body2 body1 arbiter-data))
 
 (cffi:defcallback cp-begin :int ((arb :pointer) (space :pointer) (data :pointer))
   (collision-begin arb space data))
