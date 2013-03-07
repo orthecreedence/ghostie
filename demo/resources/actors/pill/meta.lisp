95e472186cf2777be5e81094d78881d90fa42335
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-03-06 16:51:14 -0800

    making pill physics shape fit its display body a bit better

--- demo/resources/actors/pill/meta.lisp
+++ demo/resources/actors/pill/meta.lisp
@@ -1,3 +1,3 @@
 (:max-velocity 1000
- :physics ((:type :segment :mass 50d0 :position (0 0) :endpoints ((0 .8) (0 -.8)) :radius .3 :friction 0.9)))
+ :physics ((:type :segment :mass 50d0 :position (0 0) :endpoints ((0 .7) (0 -.7)) :radius .3 :friction 0.9)))
 
